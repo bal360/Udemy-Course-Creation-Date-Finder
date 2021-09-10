6671159225a7ca4d691917b6ba75c2dfd7143e6a
Blake A. Long <blake.a.long@gmail.com>
2021-09-09 21:07:09 -0600

    initial cleanup and remove references to chalk

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const layout = require('./layout')
 
 
 const port = process.env.PORT || 3000
-const server = http.createServer(async (req, res) => {
+http.createServer(async (req, res) => {
   
   let body = ''
   if (req.method === 'POST') {
@@ -14,15 +14,14 @@ const server = http.createServer(async (req, res) => {
     req.on('data', function (chunk) {
       console.log('Chunk', chunk)
       body += chunk;
-    });
+    })
+
     req.on('end', async () => {
       const parsedReq = parse(body)
       console.log('Parsed Request:', parsedReq)
       const courseURL = parsedReq.courseURL
       
-
       if (!courseURL.trim().length > 0) {
-        console.log('Give me a little something here!')
         res.end(layout({created: ''}))
       } else {
         const courseId = await getCourseId(courseURL)
@@ -30,20 +29,11 @@ const server = http.createServer(async (req, res) => {
         const date = new Date(course.created).toDateString()
         console.log('Date:', date)
         
-    
         res.writeHead(200);
         res.end(layout({created: date}));
-
       }
-
     })
   } else {
-    console.log('hit the outer else?')
     res.end(layout({created: ''}))
   }
-
-})
-
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`)
-})
+}).listen(port)
--- utils.js
+++ utils.js
@@ -1,6 +1,5 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
-const chalk = require('chalk')
 
 // +++++ PROGRAM FLOW +++++
 
@@ -13,13 +12,9 @@ const chalk = require('chalk')
     // d. Interpolate id into url for axios request to udemy api
     // e. Get course creation date from 'created' property
 
-  // == renderDate(dateString) ==
-    // f. Render creation date back to user
-
 module.exports = {
   async getCourseCreationDate(id) {
     try {
-      // const id = await getCourseId()
       const { data } = await axios.get(`https://www.udemy.com/api-2.0/courses/${id}/?fields[course]=title,url,created`)
       return data
     } catch (err) {
@@ -29,10 +24,8 @@ module.exports = {
   async getCourseId(courseURL) {
     try {
       const response = await axios.get(courseURL)
-      // console.log(chalk.gray('Course ID:'), chalk.blue(cheerio.load(response.data)('body').attr()['data-clp-course-id']))
       return cheerio.load(response.data)('body').attr()['data-clp-course-id']
     } catch (err) {
-      // throw new Error(chalk.bold.red('Verify url and try again'))
       console.log(err)
     }
   },
