81523f104a213ab42dde0d72fe37cce6e8cc811d
Blake A. Long <blake.a.long@gmail.com>
2021-09-09 21:18:18 -0600

    more CLEANUP :)

--- utils.js
+++ utils.js
@@ -1,17 +1,6 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-// +++++ PROGRAM FLOW +++++
-
-  // == getCourseId(courseURL) ==
-    // a. User submits url of course home page
-    // b. Axios request to url
-    // c. Cheerio to scrape for course id
-  
-  // == getCourseCreationDate(courseId) ==
-    // d. Interpolate id into url for axios request to udemy api
-    // e. Get course creation date from 'created' property
-
 module.exports = {
   async getCourseCreationDate(id) {
     try {
@@ -30,8 +19,6 @@ module.exports = {
     }
   },
   // splitURLForAlreadyPurchasedCourse(courseURL) {
-  //   try {
-
-  //   }
+  
   // }
 }
